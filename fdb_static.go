8be3b86d3e42c26dc76d6fdab815a489814edf81
Шестаков Денис <shestakovda@gmail.com>
2020-12-16 10:19:08 +0300

    выборка с защитой от паники

--- fdb_static.go
+++ fdb_static.go
@@ -22,27 +22,41 @@ func LoadExistingEntries(cn fdbx.Conn, db fdbx.DB, ids []string) (res []*Entry, 
 		recs = append(recs, m)
 	}
 
-	if err = db.Load(nil, recs...); err != nil {
-		// фолбэк - выбираем по одному
-		var mod Model
+	defer func() {
+		if rec := recover(); rec != nil {
+			// фолбэк - выбираем по одному
+			var mod Model
 
-		mods = mods[:0]
+			mods = mods[:0]
 
-		for i := range ids {
-			if mod, err = fac.ByID(ids[i]); err != nil {
-				glog.Infof("Ошибка загрузки Entry %s: %s", ids[i], err)
-				continue
+			for i := range ids {
+				func() {
+					defer func() {
+						if rec := recover(); rec != nil {
+							glog.Infof("Ошибка загрузки Entry %s: %s", ids[i], rec)
+						}
+					}()
+
+					if mod, err = fac.ByID(ids[i]); err != nil {
+						glog.Infof("Ошибка загрузки Entry %s: %s", ids[i], err)
+						return
+					}
+
+					mods = append(mods, mod)
+				}()
 			}
-
-			mods = append(mods, mod)
 		}
-	}
 
-	res = make([]*Entry, len(mods))
-	for i := range mods {
-		if res[i], err = mods[i].Export(true); err != nil {
-			return
+		res = make([]*Entry, len(mods))
+		for i := range mods {
+			if res[i], err = mods[i].Export(true); err != nil {
+				return
+			}
 		}
+	}()
+
+	if err = db.Load(nil, recs...); err != nil {
+		panic(err)
 	}
 
 	return res, nil
